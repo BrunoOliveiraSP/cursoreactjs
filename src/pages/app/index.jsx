aea1f270a57f9e725afa9796c7e180617a1a2fa2
Bruno O. <brunodeoliveira.22.10@gmail.com>
2024-07-04 10:34:30 -0300

    eventos

--- src/pages/app/index.jsx
+++ src/pages/app/index.jsx
@@ -8,17 +8,20 @@ export default function App() {
     <div className="pagina-app pagina">
       <header className='cabecalho'>
         <h1 className='titulo1'>
-          Estudos de ReactJS
+          Estudos de ReactJS &nbsp;
           <i className='fa fa-heart'></i>
         </h1>   
       </header>
 
       <section className='secao'>
         <h1> Estudando ReactJS</h1>
-        
+
         <ul>
           <li>
             <Link to='/contato'>Ir para Contato</Link>
+          </li>
+          <li>
+            <Link to='/eventos'>Ir para Eventos</Link>
           </li>
         </ul>
       </section>
--- src/rotas.jsx
+++ src/rotas.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './pages/app';
 import Contato from './pages/contato';
 import NaoEncontrado from './pages/naoEncontrado';
-
+import Eventos from './pages/eventos';
 
 export default function Navegacao() {
 
@@ -13,6 +13,7 @@ export default function Navegacao() {
             <Routes>
                 <Route path='/' element={<App />} />
                 <Route path='/contato' element={<Contato />} />
+                <Route path='/eventos' element={<Eventos />} />
 
                 <Route path='*' element={<NaoEncontrado />} />
             </Routes>
